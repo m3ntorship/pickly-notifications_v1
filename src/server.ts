9d4e5b23e89177d8df3d8e731af771500695fa6b
seifeldeen3388 <seifeldeen3388@gmail.com>
2020-11-11 12:32:33 +0200

    put (server listen) after (connected to db)

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,17 @@ import app from './app';
 import mongoose from 'mongoose';
 import { get } from 'config';
 
+const port = get('app.port');
+
 mongoose
 	.connect(get('app.DB_URI'), {
 		useUnifiedTopology: true,
 		useNewUrlParser: true,
 		useFindAndModify: false,
 	})
-	.then(() => console.log('connected to db'));
-
-const port = get('app.port');
-
-app.listen(port, () => {
-	console.log(`Listening on port ${port}`);
-});
+	.then(() => {
+		console.log('connected to db');
+		app.listen(port, () => {
+			console.log(`Listening on port ${port}`);
+		});
+	});
