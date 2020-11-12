3acb5f6313dd54e0c8f6938ddd829a8ad7e615de
seifeldeen3388 <seifeldeen3388@gmail.com>
2020-11-12 12:48:41 +0200

    used cors middleware

--- src/app.ts
+++ src/app.ts
@@ -8,10 +8,11 @@ import swaggerDocument from './swagger.json';
 const { protector, serviceAccount, userEnricher } = auth;
 const app: express.Application = express();
 
+app.use(cors(), express.json());
+
 app.use('/', swaggerUi.serve);
 app.get('/', swaggerUi.setup(swaggerDocument));
 
-app.use(cors(), express.json());
 app.use(protector(serviceAccount, userEnricher));
 
 app.use('/notifications', router);
